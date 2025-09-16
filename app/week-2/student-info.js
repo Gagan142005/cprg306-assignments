b7741608d1c893e9f07eda3e1bada3e7cf72aecf
Gagan142005 <gaganpreetdhillon46@gmail.com>
2025-09-16 11:10:59 -0600

    Create student-info.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Gagan</h1>
+      <Link href="https://github.com/Gagan142005">https://github.com/Gagan142005</Link>
+    </main>
+  );
+}
