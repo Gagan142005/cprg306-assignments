918db608dc70c9926cfb97e0be931b531605f887
Gagan142005 <gaganpreetdhillon46@gmail.com>
2025-09-24 12:42:58 -0600

    commit

--- app/page.js
+++ app/page.js
@@ -3,7 +3,12 @@ export default function Page() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <li>
       <Link href="/week-2">Go to Week 2</Link>
+      </li>
+      <li>
+      <Link href="/week-3">Go to Week 3</Link>
+      </li>
     </main>
   );
 }
