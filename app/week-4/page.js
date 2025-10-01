66667e648ba718d78031c8da84b2d572d54c6322
Gagan142005 <gaganpreetdhillon46@gmail.com>
2025-10-01 13:13:29 -0600

    commit

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,10 +1,15 @@
 import NewItem from "./new-item";
 
 export default function Page() {
-    return (
-        <main className=" bg-black ">
-            <h1 className="text-2xl text-white mb-4 pl-25">Week 4 Assignment</h1>
-            <NewItem/>
-        </main>
-    );
+  return (
+    <main className="min-h-screen bg-black p-6">
+      <h1 className="text-2xl font-bold text-center text-white mb-6">
+        Add New Item
+      </h1>
+      <div className="flex justify-center">
+        <NewItem />
+      </div>
+    </main>
+  );
 }
+;;
