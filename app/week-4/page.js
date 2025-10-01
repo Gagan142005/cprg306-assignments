195c4148578840c512a1be7ea3742bb53dd35c91
Gagan142005 <gaganpreetdhillon46@gmail.com>
2025-10-01 12:36:38 -0600

    commit

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -2,7 +2,7 @@ import NewItem from "./new-item";
 
 export default function Page() {
     return (
-        <main className=" bg-black p-10 ">
+        <main className=" bg-black ">
             <h1 className="text-2xl text-white mb-4 pl-25">Week 4 Assignment</h1>
             <NewItem/>
         </main>
