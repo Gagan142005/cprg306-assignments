1a6b9147085a3249107b1651901ae50804c81c11
Gagan142005 <gaganpreetdhillon46@gmail.com>
2025-11-19 12:30:54 -0700

    commit

---
+++ app/week-9/_utils/firebase.js
@@ -0,0 +1,18 @@
+
+"use client";
+
+import {initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuaeUs83Vb8E2hTc1zqZ0FvaqcOvyl4oI",
+  authDomain: "cprg306-assignments-aa202.firebaseapp.com",
+  projectId: "cprg306-assignments-aa202",
+  storageBucket: "cprg306-assignments-aa202.firebasestorage.app",
+  messagingSenderId: "693831111001",
+  appId: "1:693831111001:web:92c07a75d4f2c058f9bc48"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
